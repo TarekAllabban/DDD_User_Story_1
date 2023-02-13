8a97a6e16d49d7c553750c766af10ee3ba04e0ab
Tarek Al labban <Tarek.Allabban@nagel-group.com>
2023-02-13 12:03:28 +0100

    Add project files.

---
+++ DDD_User_Story_1/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDD_User_Story_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet(Name = "GetHello")]
+        public async Task<String> Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
